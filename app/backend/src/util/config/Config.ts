3b01b524af48e1645927be7cf33d598f9a8764c9
Abner Silva <abner.silva@clevertech.biz>
2022-05-16 13:52:24 -0300

    added model configuration for keycloak

--- app/backend/src/util/config/Config.ts
+++ app/backend/src/util/config/Config.ts
@@ -1,11 +1,23 @@
 export interface DotConfig {
+  //app db
   NODE_ENV: string;
   DATABASE_HOST: string;
   DATABASE_PORT: number;
   DATABASE_USERNAME: string;
   DATABASE_PASSWORD: string;
+
+  //TYPEORM
   TYPEORM_SYNC: boolean;
   TYPEORM_LOGGING: boolean;
-  SERVER_PORT: number;
   TYPEORM_DATABASE: string;
+  SERVER_PORT: number;
+
+  //keycloak
+  KEYCLOAK_REALM: string; // rpgManager
+  KEYCLOAK_ADMIN_PASSWORD: string;
+  KEYCLOAK_AUTH_URL: string; // http://localhost:8080/auth/ when in local
+  KEYCLOAK_RESOURCE: string; // backend-local
+  KEYCLOAK_SECRET: string;
+  KEYCLOAK_ADMIN_USER: string;
+  KEYCLOAK_ADMIN_SECRET: string;
 }
