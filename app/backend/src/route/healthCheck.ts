aa33101f84e2cec116d1d559ee1eaae3f0077eda
Abner Silva <abner.silva@clevertech.biz>
2022-05-19 11:57:34 -0300

    wip

--- app/backend/src/route/healthCheck.ts
+++ app/backend/src/route/healthCheck.ts
@@ -1,11 +1,16 @@
 import express = require("express");
+import fetch from "node-fetch";
 import di from "../di";
 
 const router = express.Router();
 
 router.get("/health_check", async (req, res) => {
-  console.log("hit routes");
-  return await di.healthCheckService.healthCheckBasic(res);
+  const response = await fetch("https://www.google.com");
+  const data = await response.json();
+
+  console.log(data);
+  // console.log("hit routes");
+  // return await di.healthCheckService.healthCheckBasic(res);
 });
 
 export { router as healthCheck };
