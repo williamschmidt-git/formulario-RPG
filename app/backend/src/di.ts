0b88b24625c986978f567f506580ceb2af3b46e7
Abner Silva <abner.silva@clevertech.biz>
2022-05-19 12:51:07 -0300

    wip

--- app/backend/src/di.ts
+++ app/backend/src/di.ts
@@ -1,44 +1,44 @@
-import { DataSource } from "typeorm";
-import { DotConfig } from "./util/config/Config";
-import { Application } from "express";
-import express = require("express");
-import { AppDataSource } from "./util/config/data-source";
-import { healthCheckService } from "./service/healthCheck";
-require("dotenv").config();
+import { DataSource } from 'typeorm';
+import { DotConfig } from './util/config/Config';
+import { Application } from 'express';
+import express = require('express');
+import { AppDataSource } from './util/config/data-source';
+import { healthCheckService } from './service/healthCheck';
+require('dotenv').config();
 
 class DependencyInjector {
-  private readonly _app: Application;
-  private readonly _env: DotConfig;
-  private _databaseConn: DataSource;
-  private _healthCheckService: healthCheckService;
+    private readonly _app: Application;
+    private readonly _env: DotConfig;
+    private _databaseConn: DataSource;
+    private _healthCheckService: healthCheckService;
 
-  constructor(env: DotConfig) {
-    this._env = env;
-    this._app = express();
-    this._healthCheckService = new healthCheckService();
+    constructor(env: DotConfig) {
+        this._env = env;
+        this._app = express();
+        this._healthCheckService = new healthCheckService();
 
-    //this._app.use(cors());
-    this._databaseConn = AppDataSource(this._env);
-  }
+        //this._app.use(cors());
+        this._databaseConn = AppDataSource(this._env);
+    }
 
-  get app(): Application {
-    return this._app;
-  }
+    get app(): Application {
+        return this._app;
+    }
 
-  get env(): DotConfig {
-    return this._env;
-  }
+    get env(): DotConfig {
+        return this._env;
+    }
 
-  get db(): DataSource {
-    return this._databaseConn;
-  }
+    get db(): DataSource {
+        return this._databaseConn;
+    }
 
-  get healthCheckService(): healthCheckService {
-    return this._healthCheckService;
-  }
+    get healthCheckService(): healthCheckService {
+        return this._healthCheckService;
+    }
 }
 
 export default (() => {
-  const DI = new DependencyInjector(process.env as unknown as DotConfig);
-  return DI;
+    const DI = new DependencyInjector(process.env as unknown as DotConfig);
+    return DI;
 })();
--- app/backend/src/route/healthCheck.ts
+++ app/backend/src/route/healthCheck.ts
@@ -1,13 +1,15 @@
 import express = require('express');
 import fetch from 'node-fetch';
+
 import di from '../di';
 
 const router = express.Router();
 
 router.get('/health_check', async (req, res) => {
     const response = await fetch('https://www.google.com');
+    //const a = got.get('www.google.com');
 
-    console.log(data);
+    //console.log(data);
     // console.log("hit routes");
     // return await di.healthCheckService.healthCheckBasic(res);
 });
