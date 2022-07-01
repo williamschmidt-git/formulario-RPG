36b8605bc44c45742648bbdde594a36e9a0a8dd1
William Schmidt <9williamschmidt@gmail.com>
2022-06-30 21:01:03 -0300

    ✨ feat: add Imodel to use on typeORM & mongoose

---
+++ app/backend/src/model/index.ts
@@ -0,0 +1,7 @@
+interface Model<T> {
+  create(obj: T): Promise<T>,
+  read(): Promise<T[]>,
+  readOne(id: string): Promise<T | null>,
+  update(id: string, obj: T): Promise<T | null>,
+  delete(id: string): Promise<T | null> 
+}
