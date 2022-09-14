3583be201bae18c97afcffdd5f61dd4b8d5a968c
William Schmidt <9williamschmidt@gmail.com>
2022-09-14 15:31:50 -0300

    🔥 remove: commented files

--- app/backend/src/util/config/data-source.ts
+++ app/backend/src/util/config/data-source.ts
@@ -2,19 +2,9 @@ import { DataSource } from 'typeorm';
 import { DotConfig } from './Config';
 import 'reflect-metadata';
 
-const AppDataSource = (env: DotConfig) =>
-  new DataSource({
-    // type: 'postgres',
-    // host: env.DATABASE_HOST,
-    // port: env.DATABASE_PORT,
-    // username: env.DATABASE_USERNAME,
-    // password: env.DATABASE_PASSWORD,
-    // database: env.TYPEORM_DATABASE,
-    // synchronize: true,
-    // logging: false,
-    // entities: [User],
-    // migrations: [],
-    // subscribers: [],
-  });
+const AppDataSource = (env: DotConfig): any => {
+  // new DataSource({});
+  return null
+}
 
 export default AppDataSource;
