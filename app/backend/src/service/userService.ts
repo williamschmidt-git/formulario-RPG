a4f02050af4d459514390d89686405ffcd8c5a15
William Schmidt <9williamschmidt@gmail.com>
2022-09-14 15:27:15 -0300

    🔥 remove: postgress config file

--- app/backend/src/service/userService.ts
+++ app/backend/src/service/userService.ts
@@ -1,5 +1,4 @@
 import di from '../di';
-import { User } from '../orm/entities/User';
 
 export class UserService {
 
--- app/backend/src/util/config/data-source.ts
+++ app/backend/src/util/config/data-source.ts
@@ -1,21 +1,20 @@
 import { DataSource } from 'typeorm';
 import { DotConfig } from './Config';
 import 'reflect-metadata';
-import { User } from '../../orm/entities/User';
 
 const AppDataSource = (env: DotConfig) =>
   new DataSource({
-    type: 'postgres',
-    host: env.DATABASE_HOST,
-    port: env.DATABASE_PORT,
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD,
-    database: env.TYPEORM_DATABASE,
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+    // type: 'postgres',
+    // host: env.DATABASE_HOST,
+    // port: env.DATABASE_PORT,
+    // username: env.DATABASE_USERNAME,
+    // password: env.DATABASE_PASSWORD,
+    // database: env.TYPEORM_DATABASE,
+    // synchronize: true,
+    // logging: false,
+    // entities: [User],
+    // migrations: [],
+    // subscribers: [],
   });
 
 export default AppDataSource;
