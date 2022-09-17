ae39aa9e94dcccd2b37729068674aa675886e7fd
William Schmidt <9williamschmidt@gmail.com>
2022-09-17 13:43:28 -0300

    ✨ docker-compose properly functioning with mongodb

--- app/backend/src/model/MongoConnection.ts
+++ app/backend/src/model/MongoConnection.ts
@@ -1,12 +1,12 @@
 import { connect } from 'mongoose';
 
-// const options = {
-//   user: 'user', // Usuário do banco de dados.
-//   pass: 'password', // senha do usuário do banco de dados.
-//   autoIndex: false, // Não cria index para cada inserção de dado no banco.
-//   dbName: 'glassesStore', // Define qual banco de dados vou utilizar.
-// };
+const options = {
+  user: 'user', // Usuário do banco de dados.
+  pass: 'password', // senha do usuário do banco de dados.
+  // autoIndex: false, // Não cria index para cada inserção de dado no banco.
+  // dbName: 'glassesStore', // Define qual banco de dados vou utilizar.
+};
 
-connect('mongodb://localhost:27017/').then(() => {
+connect('db_mongo://db:27017/', options).then(() => {
   console.log('successfully connected')
 });
