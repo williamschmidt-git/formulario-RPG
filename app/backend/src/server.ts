223ff613c1b47bbae2873e647d13d499c1f9c852
William Schmidt <9williamschmidt@gmail.com>
2022-09-20 17:21:28 -0300

    ✨ change destructuring from server

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,8 +1,10 @@
 import di from './di';
 import { routes } from './route';
 
-di.app.listen(di.env.SERVER_PORT, () => {
-    console.log(`Server is listening on port: ${di.env.SERVER_PORT}`);
+const { SERVER_PORT } = di.env
+
+di.app.listen(SERVER_PORT, () => {
+    console.log(`Server is listening on port: ${SERVER_PORT}`);
 });
 
 di.app.use(routes);
