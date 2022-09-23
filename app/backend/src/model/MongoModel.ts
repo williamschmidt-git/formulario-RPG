09d45e865ea4bccad9a3b6a6343b30e071456ee1
William Schmidt <9williamschmidt@gmail.com>
2022-09-23 17:58:10 -0300

    ✨ changed some stuff

--- app/backend/src/model/MongoModel.ts
+++ app/backend/src/model/MongoModel.ts
@@ -1,18 +1,21 @@
-// import { Model as M, Document } from 'mongoose';
-// import { Model } from '../interfaces/ModelInterface';
+import { Model as M, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
 
-//   abstract class MongoModel<T> implements Model<T> {
-//     constructor(protected model: M<T & Document>) { }
+  abstract class MongoModel<T> implements Model<T> {
+    constructor(protected model: M<T & Document>) { }
 
-//     create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+    create = async (obj: T): Promise<T> => this.model.create({ ...obj });
 
-//     read = async (): Promise<T[]> => this.model.find();
+    read = async (): Promise<T[]> => this.model.find();
 
-//     readOne = async (id: string): Promise<T | null> =>
-//       this.model.findOne({ _id: id });
+    readOne = async (id: string): Promise<T | null> =>
+      this.model.findOne({ _id: id });
 
-//     //update
-//     //delete
-//   }
+    update = async (id: string, obj: T): Promise<T | null> =>
+      this.model.findByIdAndUpdate({_id: id}, { $set: obj})
 
-//   export default MongoModel;
+    delete = async (id: string): Promise<T | null> =>
+      this.model.findByIdAndDelete(id)
+  }
+
+  export default MongoModel;
