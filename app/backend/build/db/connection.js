dc196590ebf68de413e4776e7ce8143f9d4a2532
williamschmidt-git <9williamschmidt@gmail.com>
2022-10-18 17:11:20 -0300

    teste

--- app/backend/build/db/connection.js
+++ app/backend/build/db/connection.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -22,14 +13,16 @@ const mongoose_1 = __importDefault(require("mongoose"));
 // mongoose.connect('db_mongo://db:27017/', options).then(() => {
 //   console.log('successfully connected')
 // });
-const connectToDatabase = () => __awaiter(void 0, void 0, void 0, function* () {
-    // eslint-disable-next-line max-len
-    return yield mongoose_1.default.connect('mongodb+srv://rojao:13tiro@rojao.covwnst.mongodb.net/test').then(() => {
-        console.log('Foi!!!!!!!!!!!!!!!!!!!!!!!!!');
-    })
-        .catch((e) => {
-        console.log(e);
-    });
+const connectToDatabase = () => 
+// eslint-disable-next-line max-len
+mongoose_1.default.connect('mongodb+srv://rojao:13tiro@rojao.covwnst.mongodb.net/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Foi!!!!!!!!!!!!!!!!!!!!!!!!!');
+})
+    .catch((e) => {
+    console.log(e);
 });
 exports.default = connectToDatabase;
 // mongodb://username:password@host:port/db
