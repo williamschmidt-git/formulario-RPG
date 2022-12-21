504684d016b0e0fabae85feaaec25bf58aa1c294
williamschmidt-git <9williamschmidt@gmail.com>
2022-12-21 19:46:28 -0300

    revert

--- app/frontend/src/src/App.tsx
+++ app/frontend/src/src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       oi
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of 115b047... cleaning some folders
     </div>
   );
 }
